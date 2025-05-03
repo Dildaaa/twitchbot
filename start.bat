fc57f9e52291d797d5a8b788a1bb7cfcf2f9982b
Denys Daneliuk <d.danelyuk@gmail.com>
2025-05-03 17:27:40 +0200

    fix

--- start.bat
+++ start.bat
@@ -1,14 +1,6 @@
 @echo off
 SETLOCAL
 
-:: Вказати назву папки репозиторію
-set REPO_DIR="twitchbot"
-
-:: Перейти в директорію
-cd /d "%~dp0%REPO_DIR%"
-
-:: Запустити бота
-echo Running bot.py...
 python bot.py
 
 pause
